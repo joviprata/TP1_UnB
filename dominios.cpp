c88b21f94963c7fdea59f8531dd77f5d05f2e4f5
Jovi Prata <joviprata07@gmail.com>
2024-10-24 13:48:08 -0300

    criação de funções para classe Nome

--- dominios.cpp
+++ dominios.cpp
@@ -1,13 +1,22 @@
 #include "dominios.hpp"
 
+// Função para classe Horario, Data, etc
+bool string_eh_int(string str) {
+    for (char chr : str) {
+        if (!isdigit(chr))
+            return false;
+    }
+    return true;
+}
+
+// Funções para classe Horario:
 bool Horario::validar(string horario) {
     if (horario.size() != 5 or horario[2] != ':')
         return false;
 
     string str_HH = horario.substr(0, 2);
     string str_mm = horario.substr(3, 2);
-    if (!isdigit(str_HH[0]) or !isdigit(str_HH[1])
-        or !isdigit(str_mm[0]) or !isdigit(str_mm[1]))
+    if (!string_eh_int(str_HH) or !string_eh_int(str_mm))
         return false;
 
     int HH = stoi(str_HH);
@@ -22,5 +31,20 @@ bool Horario::setHorario(string horario) {
     if(!validar(horario))
         return false;
     this->horario = horario;
+    return true;
+}
+
+
+// Funções para classe Nome:
+bool Nome::validar(string nome) {
+    if (nome.size() > 30)
+        return false;
+    return true;
+}
+
+bool Nome::setNome(string nome) {
+    if(!validar(nome))
+        return false;
+    this->nome = nome;
     return true;
 }

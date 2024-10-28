7a7bcc029310c75640c0e57492c114ad9e2fb9cd
Isabela S. Furlan <isabelasfurlan@gmail.com>
2024-10-28 16:52:12 -0300

    Update dominios.hpp

--- dominios.hpp
+++ dominios.hpp
@@ -1,6 +1,7 @@
 #ifndef DOMINIOS_HPP_INCLUDED
 #define DOMINIOS_HPP_INCLUDED
 
+#include <stdexcept> // Facilita tratar erros como de argumento inválido
 #include <bits/stdc++.h> // Importa bibliotecas padrao de C++ - iostream, vector, string, etc
 using namespace std; // Facilita ao tornar desnecessario escrever std:: antes de classes padrao do C++
 
@@ -86,5 +87,23 @@ class Avaliacao {
 inline int Avaliacao::getAvaliacao() const{
     return avaliacao;
 }
+
+class Duracao {
+    public:
+        // Inicia com 0 como valor da duracao
+        Duracao(int duracao = 0);
+
+        void setDuracao(int duracao);
+        
+        inline int getDuracao() const;
+
+    private:
+        int duracao;
+};
+
+inline int Duracao::getDuracao() const {
+    return duracao;
+}
+
 
 #endif // DOMINIOS_HPP_INCLUDED

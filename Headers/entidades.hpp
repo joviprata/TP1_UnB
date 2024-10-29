dfc7a3edd095c41eb4346a3435d573fab4383cff
Jovi Prata <joviprata07@gmail.com>
2024-10-29 19:04:17 -0300

    adição de entidade Viagem

--- Headers/entidades.hpp
+++ Headers/entidades.hpp
@@ -3,6 +3,47 @@
 
 using namespace std;
 
+class Viagem{
+    private:
+        Codigo codigo;
+        Nome nome;
+        Avaliacao avaliacao;
+    public:
+        void setCodigo(const Codigo&);
+        Codigo getCodigo() const;
+
+        void setNome(const Nome&);
+        Nome getNome() const;
+
+        void setAvaliacao(const Avaliacao&);
+        Avaliacao getAvaliacao() const;
+};
+
+inline void Viagem::setCodigo(const Codigo &codigo){
+    this->codigo = codigo;
+}
+
+inline void Viagem::setNome(const Nome &nome){
+    this->nome = nome;
+}
+
+inline void Viagem::setAvaliacao(const Avaliacao &avaliacao){
+    this->avaliacao = avaliacao;
+}
+
+inline Codigo Viagem::getCodigo() const{
+    return codigo;
+}
+
+inline Nome Viagem::getNome() const{
+    return nome;
+}
+
+inline Avaliacao Viagem::getAvaliacao() const{
+    return avaliacao;
+}
+
+
 class Destino {
     private:
         Codigo codigo;

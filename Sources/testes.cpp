f3c73a411bf8aff358388b5bd8fe4a7c5f34eee9
MauroSon <127454440+MauroSon@users.noreply.github.com>
2024-11-26 11:12:12 -0300

    Update testes.cpp

--- Sources/testes.cpp
+++ Sources/testes.cpp
@@ -1,1 +1,35 @@
-
+#include "testes.hpp"
+void TUAvaliacao::setUp(){
+    avaliacao = new Avaliacao();
+    estado = SUCESSO;
+}
+void TUAvaliacao::tearDown(){
+    delete avaliacao;
+}
+void TUAvaliacao::testarCenarioValorValido(){
+    try{
+        avaliacao->setAvaliacao(5);
+        if (avaliacao->getAvaliacao() != 5)
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+    estado = FALHA;
+    }
+}
+void TUAvaliacao::testarCenarioValorInvalido(){
+    try{
+        avaliacao->setAvaliacao(6);
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        if (avaliacao->getAvaliacao() == 6)
+        estado = FALHA;
+    }
+}
+int TUAvaliacao::run(){
+    setUp();
+    testarCenarioValorValido();
+    testarCenarioValorInvalido();
+    tearDown();
+    return estado;
+}

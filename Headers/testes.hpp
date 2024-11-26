ed794f2a67a58013a1168beb89cb9f981e6fd767
Jovi Prata <joviprata07@gmail.com>
2024-11-26 11:11:30 -0300

    criação de TUHorario

--- Headers/testes.hpp
+++ Headers/testes.hpp
@@ -1,6 +1,28 @@
 #ifndef TESTES_HPP_INCLUDED
 #define TESTES_HPP_INCLUDED
 
+#include <stdexcept>
+
+#include "dominios.hpp"
+
+using namespace std;
+
+class TUHorario {
+private:
+    const string HORARIO_VALIDO = "12:34";
+    const string HORARIO_INVALIDO = "AB:CD";
+    Horario *horario;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarCenarioHorarioValido();
+    void testarCenarioHorarioInvalido();
+
+public:
+    const static int SUCESSO = 0;
+    const static int FALHA = -1;
+    int run();
+};
 
 
 #endif // TESTES_HPP_INCLUDED

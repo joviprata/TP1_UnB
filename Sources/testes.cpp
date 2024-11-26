49ea638272b694ed9c00e5fbf6ba0cf295102f85
Jovi Prata <joviprata07@gmail.com>
2024-11-26 11:25:34 -0300

    adição de TUNome

--- Sources/testes.cpp
+++ Sources/testes.cpp
@@ -1,5 +1,8 @@
 #include "testes.hpp"
 
+
+// Teste Unitário Horario:
+
 void TUHorario::setUp(){
     horario = new Horario();
     estado = SUCESSO;
@@ -35,6 +38,48 @@ int TUHorario::run(){
     setUp();
     testarCenarioHorarioValido();
     testarCenarioHorarioInvalido();
+    tearDown();
+    return estado;
+}
+
+
+// Teste Unitário Nome:
+
+void TUNome::setUp(){
+    nome = new Nome();
+    estado = SUCESSO;
+}
+
+void TUNome::tearDown(){
+    delete nome;
+}
+
+void TUNome::testarCenarioNomeValido(){
+    try{
+        nome->setNome(NOME_VALIDO);
+        if(nome->getNome() != NOME_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        estado = FALHA;
+    }
+}
+
+void TUNome::testarCenarioNomeInvalido(){
+    try{
+        nome->setNome(NOME_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        if(nome->getNome() == NOME_INVALIDO)
+            estado = FALHA;
+    }
+}
+
+int TUNome::run(){
+    setUp();
+    testarCenarioNomeValido();
+    testarCenarioNomeInvalido();
     tearDown();
     return estado;
 }

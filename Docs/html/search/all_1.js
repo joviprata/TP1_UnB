7d672f0d321beb230db5afea2f6abf383452a6c8
masolivs <marianasoli621@gmail.com>
2024-11-30 20:08:50 -0300

    Alterações na documentação do projeto.

--- Docs/html/search/all_1.js
+++ Docs/html/search/all_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['codigo_0',['Codigo',['../classCodigo.html',1,'']]],
-  ['codigo_1',['codigo',['../classCodigo.html#a13505d54d829b35a37ed8bd62a84f0cf',1,'Codigo::codigo'],['../classViagem.html#a739e0ba126961d446778e8ec2bd94806',1,'Viagem::codigo'],['../classDestino.html#a7d65862186dc2e1f8b5690a93dc3859d',1,'Destino::codigo'],['../classConta.html#ade15405c2df012eb02b0c307d6ddad6d',1,'Conta::codigo'],['../classHospedagem.html#a73c5d64f045294111ead9051146c5aeb',1,'Hospedagem::codigo'],['../classAtividades.html#a4719a9a20bfaf355719c1ddca4b6c277',1,'Atividades::codigo']]],
-  ['conta_2',['Conta',['../classConta.html',1,'']]]
+  ['conta_1',['Conta',['../classConta.html',1,'']]]
 ];
--- Docs/html/search/variables_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['nome_0',['nome',['../classNome.html#ac3f8577d2bfac039271cc13c1c570df8',1,'Nome::nome'],['../classViagem.html#a8874bee730ca25775e972f415cbc4d65',1,'Viagem::nome'],['../classDestino.html#a94a01165f2f3bc5f7dd845396e21c530',1,'Destino::nome'],['../classHospedagem.html#a1eaad3192d187cccf43295b4039c73e4',1,'Hospedagem::nome'],['../classAtividades.html#a203c868a01ce92e9a03c7bee7d625141',1,'Atividades::nome']]]
-];

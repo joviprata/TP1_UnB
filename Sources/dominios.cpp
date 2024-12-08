fea7319f7676f94d6163441214c02a1acb42d94a
Jovi Prata <joviprata07@gmail.com>
2024-12-08 15:49:46 -0300

    alterações gerais

--- Sources/dominios.cpp
+++ Sources/dominios.cpp
@@ -1,16 +1,111 @@
 #include "dominios.hpp"
 #include <cctype> // para isdigit e isalpha
 #include <iostream>
-#include <stdexcept> // Facilita tratar erros como de argumento inválido
+#include <stdexcept> // facilita tratar erros como de argumento inválido
 using namespace std;
 
-// Função para classe Horario, Data, etc
+// Função auxiliar usada nas classes Data e Horario:
 bool string_eh_int(string str) {
     for (char chr : str) {
         if (!isdigit(chr))
             return false;
     }
     return true;
+}
+
+
+// Funções para classe Avaliacao:
+void Avaliacao::validar(int avaliacao){
+    if ((avaliacao < 0) or (avaliacao > 5))
+        throw invalid_argument("Argumento invalido");
+}
+
+void Avaliacao::setAvaliacao(int avaliacao){
+    validar(avaliacao);
+    this->avaliacao = avaliacao;
+}
+
+
+// Funções para a classe Codigo:
+void Codigo::validar(string codigo) {
+    if (codigo.size() != 6)
+        throw invalid_argument("Argumento invalido");
+
+    for (char c: codigo)
+        if (!isalnum(c)) // isalnum identifica se o caracter é alfanumérico (A-Z, a-z ou 0-9)
+            throw invalid_argument("Argumento invalido");
+}
+
+void Codigo::setCodigo(string codigo) {
+    validar(codigo);
+    this->codigo = codigo;
+}
+
+
+// Funções para classe Data:
+void Data::validar(string data){
+    if (data.size() != 8 or (data[2] != '-' or data[5] != '-'))
+          throw invalid_argument("Argumento invalido");
+
+    string str_DD = data.substr(0,2);
+    string str_MM = data.substr(3,2);
+    string str_AA = data.substr(6,2);
+
+    if (!string_eh_int(str_DD) or !string_eh_int(str_MM) or !string_eh_int(str_AA))
+         throw invalid_argument("Argumento invalido");
+
+    int DD = stoi(str_DD);
+    int MM = stoi(str_MM);
+    int AA = stoi(str_AA);
+
+     // Verifica se o mês é válido:
+    if (DD < 1 or DD > 31 or MM < 1 or MM > 12 or AA < 0)
+         throw invalid_argument("Argumento invalido");
+
+    // Verifica dias válidos para cada mês:
+    if ((MM == 2 or MM == 4 or MM == 6 or MM == 9 or MM == 11) and DD > 30){
+         throw invalid_argument("Argumento invalido");
+    }
+
+    // Verifica novamente o mês de fevereiro considerando anos bissextos:
+    if (MM == 2){
+        bool bissexto = (AA % 4 == 0 and (AA % 100 != 0 or AA % 400 == 0));
+        if ((bissexto and DD > 29) or (!bissexto and DD > 28)){
+                 throw invalid_argument("Argumento invalido");
+        }
+    }
+}
+
+void Data::setData(string data){
+    validar(data);
+    this->data = data;
+
+}
+
+
+// Funções para a classe Dinheiro:
+void Dinheiro::validar(float dinheiro) {
+    if (dinheiro < 0.0 or dinheiro > 200000.0) {
+        throw invalid_argument("Argumento invalido");
+    }
+}
+
+void Dinheiro::setDinheiro(float dinheiro) {
+    validar(dinheiro);
+    this->dinheiro = dinheiro;
+}
+
+
+// Funções para a classe Duracao:
+void Duracao::validar(int duracao) {
+    if (duracao < 0 or duracao > 360) {
+        throw invalid_argument("Argumento invalido");
+    }
+}
+
+void Duracao::setDuracao(int duracao) {
+    validar(duracao);
+    this->duracao = duracao;
 }
 
 
@@ -68,114 +163,12 @@ void Senha::validar(string senha) {
         contador++;
     }
 
-
-    if((!duplicata && !crescente && !decrescente) == false){
+    if((!duplicata and !crescente and !decrescente) == false){
         throw invalid_argument("Argumento invalido.");
     }
-
-
 }
 
 void Senha::setSenha(string senha) {
     validar(senha);
     this->senha = senha;
-
-}
-
-
-// Funções para classe data:
-void Data::validar(string data){
-    if (data.size() != 8 or (data[2] != '-' or data[5] != '-'))
-          throw invalid_argument("Argumento invalido");
-
-    string str_DD = data.substr(0,2);
-    string str_MM = data.substr(3,2);
-    string str_AA = data.substr(6,2);
-
-    if (!string_eh_int(str_DD) or !string_eh_int(str_MM) or !string_eh_int(str_AA))
-         throw invalid_argument("Argumento invalido");
-
-    int DD = stoi(str_DD);
-    int MM = stoi(str_MM);
-    int AA = stoi(str_AA);
-
-     // Verifica se o mes e valido
-    if (DD > 31 or MM > 12 or DD <= 0 or MM < 1)
-         throw invalid_argument("Argumento invalido");
-
-    // Verifica dias validos para cada mes
-    if ((MM == 4 or MM == 6 or MM == 9 or MM == 11 or MM == 2)and DD > 30){
-         throw invalid_argument("Argumento invalido");
-    }
-
-    if (MM == 2){
-        bool bissexto = (AA % 4 == 0 and (AA % 100 != 0 or AA % 400 == 0));
-        if ((bissexto and DD > 29) or (!bissexto and DD > 28)){
-                 throw invalid_argument("Argumento invalido");
-        }
-    }
-
-
-}
-
-void Data::setData(string data){
-    validar(data);
-    this ->data = data;
-
-}
-
-// Funções para a classe Codigo:
-void Codigo::validar(string codigo) {
-
-    if (codigo.size() != 6)
-        throw invalid_argument("Codigo invalido");
-
-    for (char c: codigo)
-        if (!isalnum(c)) // isalnum identifica se o caracter é alfanumérico (A-Z, a-z ou 0-9)
-            throw invalid_argument("Codigo Invalido");
-}
-
-void Codigo::setCodigo (string codigo) {
-    validar(codigo);
-    this->codigo = codigo;
-}
-
-//Funções para classe Avaliacao:
-void Avaliacao::validar(int avaliacao){
-
-    if ((avaliacao < 0) || (avaliacao>5))
-        throw invalid_argument("Argumento Invalido");
-}
-
-void Avaliacao::setAvaliacao(int avaliacao){
-    validar(avaliacao);
-    this->avaliacao = avaliacao;
-}
-
-
-// Funções para a classe Duracao
-void Duracao::validar(int duracao) {
-    // Verifica se duracao maior que 0 e menor que 360, ou seja, válida
-    if (duracao < 0 || duracao > 360) {
-        throw invalid_argument("Argumento invalido");
-    }
-}
-
-void Duracao::setDuracao(int duracao) {
-    validar(duracao);
-    this->duracao = duracao;
-
-}
-
-// Funções para a classe Dinheiro
-void Dinheiro::validar(double dinheiro) {
-    // Apenas valore maiores que 0,00 e menores ou iguais a 200000,00
-    if (dinheiro < 0.00 || dinheiro > 200000.00) {
-        throw invalid_argument("Argumento invalido");
-    }
-}
-
-void Dinheiro::setDinheiro(double dinheiro) {
-    validar(dinheiro);
-    this->dinheiro = dinheiro;
 }

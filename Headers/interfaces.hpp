a31d0aa65b5983aad61d96cf768165d22a3da581
MauroSon <127454440+MauroSon@users.noreply.github.com>
2025-01-27 10:37:37 -0300

    Fixes interfaces.hpp

--- Headers/interfaces.hpp
+++ Headers/interfaces.hpp
@@ -9,9 +9,14 @@ using namespace std;
 
 // INL = InterfaceLogicaNegocio
 
+/**
+ * @brief Foraward declaration (Declaração adiantada)
+*/
+
 class InterfaceLogicaNegocioEntidadesServico;
 class InterfaceEntidadesApresentacao;
-class ILNContaServico; 
+class ILNContaServico;
+class ILNSAutenticacao;
 
 class InterfaceEntidadesServico {
 public:
@@ -20,10 +25,10 @@ public:
     virtual bool ler(Viagem*) = 0;
     virtual bool atualizar(Viagem) = 0;
 
-    virtual bool criar(Atividade) = 0;
-    virtual bool excluir(Atividade) = 0;
-    virtual bool ler(Atividade*) = 0;
-    virtual bool atualizar(Atividade) = 0;
+    virtual bool criar(Atividades) = 0;
+    virtual bool excluir(Atividades) = 0;
+    virtual bool ler(Atividades*) = 0;
+    virtual bool atualizar(Atividades) = 0;
 
     virtual bool criar(Destino) = 0;
     virtual bool excluir(Destino) = 0;
@@ -46,10 +51,10 @@ public:
     virtual bool ler(const Viagem&) = 0;
     virtual bool atualizar(const Viagem&) = 0;
 
-    virtual bool criar(const Atividade&) = 0;
-    virtual bool excluir(const Atividade&) = 0;
-    virtual bool ler(const Atividade&) = 0;
-    virtual bool atualizar(const Atividade&) = 0;
+    virtual bool criar(const Atividades&) = 0;
+    virtual bool excluir(const Atividades&) = 0;
+    virtual bool ler(const Atividades&) = 0;
+    virtual bool atualizar(const Atividades&) = 0;
 
     virtual bool criar(const Destino&) = 0;
     virtual bool excluir(const Destino&) = 0;
@@ -79,19 +84,15 @@ class InterfaceEntidadesApresentacao{
 public:
     virtual void executar(Codigo) = 0;
     virtual ~InterfaceEntidadesApresentacao(){};
-}
+};
 
 // --------------------------------------------------------------------
 // INTERFACE SERVIÇO AUTENTICAÇÃO
-/** 
- * @brief Foraward declaration (Declaração adiantada) da interface ILNSAutenticacao
-*/
-class ILNSAutenticacao;
 
-/** 
+/**
  * @brief Classe abstrata Interface Serviço Autenticação
- * 
- * 
+ *
+ *
 */
 class IServicoAutenticacao {
     public:
@@ -101,9 +102,9 @@ class IServicoAutenticacao {
         virtual ~IServicoAutenticacao() {} ///< Método destrutor da classe.
 };
 
-/** 
+/**
  * @brief Classe abstrata da lógica de negócio Interface Serviço Autenticação
- * 
+ *
 */
 class ILNSAutenticacao {
     public:
@@ -129,7 +130,7 @@ public:
     virtual bool ler(const Conta&) = 0;
     virtual bool atualizar(const Conta&) = 0;
 
-    virtual ~INEContaServico(){};
+    virtual ~INLContaServico(){};
 };
 
 #endif // INTERFACES_HPP_INCLUDED

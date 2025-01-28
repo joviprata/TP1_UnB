fac2df5d3d073cbc06bd11447715e72711b0ef71
BalderLink <142056083+BalderLink@users.noreply.github.com>
2025-01-28 11:14:21 -0300

    Adição StubContaServico em stubs.hpp

--- Headers/stubs.hpp
+++ Headers/stubs.hpp
@@ -20,4 +20,21 @@ public:
     bool autenticar(const Codigo&, const Senha&);
 };
 
+// Classe StubContaServiço implementa a interface IContaServiço.
+
+class StubContaServico:public IContaServico{
+
+private:
+
+    const static string TRIGGER_FALHA_AUTENTICACAO;
+    const static string TRIGGER_ERRO_SISTEMA_AUTENTICACAO;
+
+public:
+
+    bool criar(Conta*);
+    bool excluir(Codigo*):
+    bool ler(Conta*);
+    bool atualizar(Conta*);
+};
+
 #endif // STUBS_HPP_INCLUDED
